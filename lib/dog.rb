be2d81e24037e9a5374eb4bf731634dbb48ac9a3
Donna Torres <todonnatorres@icloud.com>
2020-06-04 11:05:40 +0000

    Done.

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,1 +1,20 @@
 # Your code goes here!
+require 'pry'
+class Dog
+  def name=(name)
+    @name = name
+  end
+  
+  def name
+    @name
+  end
+  
+  def bark
+    puts "woof!"
+  end
+    
+end
+ 
+
+
+
